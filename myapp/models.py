add916d9d9fccea8c3937fa43418c46ca3d57cd8
maggie <qi.tian@zongmutech.com>
2017-11-27 16:42:59 +0800

    add model field

--- myapp/models.py
+++ myapp/models.py
@@ -3,3 +3,15 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_txt=models.CharField(max_length=200)
+    pub_data=models.DateField('date published')
+
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_txt = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+
