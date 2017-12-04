dc4e3dbf5231319aa71b238308f9b224b9c9f680
Maggie Tian <1729430864@qq.com>
2017-12-04 23:44:13 +0800

    add html file

--- myapp/urls.py
+++ myapp/urls.py
@@ -10,4 +10,9 @@ from . import views
 
 urlpatterns=[
     url(r'^$',views.index,name='index'),
+    #url(r'^(?P<question_id>[0-9]+)/$',views.detail,name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultView.as_view(), name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
